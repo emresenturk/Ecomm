44752693bad80084ee1cdbb7402b26b1093cd0eb
Emre Senturk <m.emre.senturk@gmail.com>
2016-07-04 18:45:57 +0300

    Development of commerce service models started.

---
+++ Ecomm.Commerce/Data/ShoppingCart.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace Ecomm.Commerce.Data
+{
+    public class ShoppingCart
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int? UserId { get; set; }
+
+        public virtual ICollection<ShoppingCartItem> Items { get; set; }
+
+        [NotMapped]
+        public decimal TotalPrice
+        {
+            get { return Items.Sum(i => i.TotalPrice); }
+        }
+
+        [NotMapped]
+        public decimal TotalPriceBeforeTax
+        {
+            get { return Items.Sum(i => i.TotalPriceBeforeTax); }
+        }
+
+        [NotMapped]
+        public decimal TaxAmount
+        {
+            get { return Items.Sum(i => i.TaxAmount); }
+        }
+    }
+}
---
+++ Ecomm.Commerce/Data/ShoppingCartItem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ecomm.Commerce.Data
+{
+    public class ShoppingCartItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ShoppingCartId { get; set; }
+
+        public string ERPCode { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public decimal TaxRatio { get; set; }
+
+        public int Quantity { get; set; }
+
+        [ForeignKey("ShoppingCartId")]
+        public virtual ShoppingCart Cart { get; set; }
+
+
+        [NotMapped]
+        public decimal UnitPriceAfterTax
+        {
+            get { return Math.Round(UnitPrice*TaxRatio, MidpointRounding.AwayFromZero) + UnitPrice; }
+        }
+
+        [NotMapped]
+        public decimal TaxAmount
+        {
+            get { return Math.Round(UnitPrice*Quantity*TaxRatio, MidpointRounding.AwayFromZero); }
+        }
+
+        [NotMapped]
+        public decimal TotalPriceBeforeTax
+        {
+            get { return UnitPrice*Quantity; }
+        }
+
+        [NotMapped]
+        public decimal TotalPrice
+        {
+            get { return TotalPriceBeforeTax + TaxAmount; }
+        }
+    }
+}
